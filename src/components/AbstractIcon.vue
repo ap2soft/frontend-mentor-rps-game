ffabd86bc39c80668ceb71f51d2f504f8f3109e2
Andrew Ponomarev <spargo32@gmail.com>
2022-05-14 16:56:34 +0300

    Figures are buttons

--- src/components/AbstractIcon.vue
+++ src/components/AbstractIcon.vue
@@ -8,14 +8,14 @@ const props = defineProps([
 </script>
 
 <template>
-  <div
-    class="h-28 w-28 rounded-full bg-gradient-to-b p-3 shadow-bottom tablet:h-40 tablet:w-40 tablet:p-5"
+  <button
+    class="group h-28 w-28 rounded-full bg-gradient-to-b p-3 shadow-bottom tablet:h-40 tablet:w-40 tablet:p-5"
     :class="`${gradientColors} ${shadowColors}`"
   >
     <div
-      class="grid h-full w-full place-content-center rounded-full bg-gradient-to-b from-[#dadada] to-[#f2f2f2] p-6 shadow-top shadow-[#babed2] tablet:p-2"
+      class="grid h-full w-full place-content-center rounded-full bg-white bg-gradient-to-b from-[#dadada] to-[#f2f2f2] p-6 shadow-top shadow-[#babed2] transition group-hover:bg-none tablet:p-2"
     >
       <img :src="iconPath" :alt="altText" class="h-10 tablet:h-16" />
     </div>
-  </div>
+  </button>
 </template>
--- src/components/Game.vue
+++ src/components/Game.vue
@@ -12,18 +12,22 @@ import Rock from './Rock.vue'
   >
     <Header />
 
-    <div
-      class="relative mx-auto h-52 w-52 bg-triangle bg-contain bg-center bg-no-repeat tablet:h-64 tablet:w-64"
-    >
-      <Paper class="absolute -left-10 -top-8 tablet:-left-16 tablet:-top-16" />
-      <Scissors
-        class="absolute -right-10 -top-8 tablet:-right-16 tablet:-top-16"
-      />
-      <Rock
-        class="absolute left-1/2 -bottom-8 -translate-x-1/2 tablet:-bottom-12"
-      />
-    </div>
+    <main>
+      <div
+        class="relative mx-auto h-52 w-52 bg-triangle bg-contain bg-center bg-no-repeat tablet:h-64 tablet:w-64"
+      >
+        <Paper
+          class="absolute -left-10 -top-8 tablet:-left-16 tablet:-top-16"
+        />
+        <Scissors
+          class="absolute -right-10 -top-8 tablet:-right-16 tablet:-top-16"
+        />
+        <Rock
+          class="absolute left-1/2 -bottom-8 -translate-x-1/2 tablet:-bottom-12"
+        />
+      </div>
+    </main>
 
-    <Rules class="tablet:absolute right-10 bottom-10" />
+    <Rules class="right-10 bottom-10 tablet:absolute" />
   </div>
 </template>
